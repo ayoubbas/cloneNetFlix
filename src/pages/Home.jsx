1c713c256b96d8856bdebddf39824b602bfa2790
ayoub <ayoub-ok147@hotmail.fr>
2023-08-25 15:23:55 +0100

    new

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
     <Main />
     <Row title="Up comming" fetchUrl={requests.requestUpcoming} />
     <Row title="Papular" fetchUrl={requests.requestPopular} />
-    <Row title="" fetchUrl={requests.requestHorror} />
-    <Row title="Up comming" fetchUrl={requests.requestTopRated} />
+    <Row title="Horror" fetchUrl={requests.requestHorror} />
+    <Row title="Top Rated" fetchUrl={requests.requestTopRated} />
     <Row title="Up comming" fetchUrl={requests.requestTrending} />
     </>
   )
